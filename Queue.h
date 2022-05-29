073f23edee1f17612a40ce739849a09b34868765
AbdulhadiYasin <Abdulhadi.yasin@outlook.com>
2022-05-29 18:31:46 +0300

    Embeded QueueNode.h into Queue.h, added unit tests.

--- Queue.h
+++ Queue.h
@@ -10,8 +10,119 @@
 
 #include <stdio.h>
 #include <iostream>
-#include "QueueNode.h"
 using namespace std;
+
+template <class T> class QueueNode {
+public:
+    /**
+     * C'tor of QueueNode class
+     *
+     * @param value - The value of the node.
+    */
+    QueueNode(T value);
+    
+    /**
+     * D'tor of Queue class
+    */
+    ~QueueNode();
+    
+    /*
+     * Assignment operator of QueueNode class.
+     *
+     * @param other - reference to an QueueNode object.
+     * @return
+     *      reference to an QueueNode object.
+     */
+    QueueNode& operator=(const QueueNode& other) = default;
+    
+    /**
+     * Returns node's value.
+     *
+     * @return Value of the receiver.
+    */
+    T& getValue();
+    
+    /**
+     * Sets the receiver (Node) to a new value.
+     *
+     * @param newValue - The new value to set.
+    */
+    void setValue(const T& newValue);
+    
+    /**
+     * Adds a new node after the receiver with the provided newValue.
+     *
+     * @param value - The value to add as the next node of the receiver.
+     * @return A reference to the node after the receiver after the update.
+    */
+    QueueNode* addNext(const T& value);
+    
+    /**
+     * Returns reference to the node after the receiver.
+     *
+     * @return A reference to the node after the receiver, if there's no such node returns NULL.
+    */
+    QueueNode* getNext() const;
+    
+    /**
+     * Sets the node after the receiver (Node) to be a new node.
+     * @note You are responsible of deleting the previous `next` when needed.
+     *
+     * @param newNext - The other node to set as next of the receiver.
+    */
+    void setNext(QueueNode* newNext);
+    
+    
+private:
+    T m_value;
+    QueueNode<T>* m_next;
+};
+
+
+
+template<class T> QueueNode<T>::QueueNode(T value): m_value(value), m_next(NULL) {
+    m_next = NULL;
+}
+
+template<class T> QueueNode<T>::~QueueNode() {
+    delete m_next;
+}
+    
+template<class T> T& QueueNode<T>::getValue() {
+    return m_value;
+}
+
+template<class T> void QueueNode<T>::setValue(const T& newValue) {
+    m_value = newValue;
+}
+
+template<class T> QueueNode<T>* QueueNode<T>::getNext() const {
+    return m_next;
+}
+
+template<class T> QueueNode<T>* QueueNode<T>::addNext(const T& value) {
+    if(m_next == NULL){
+        QueueNode<T> *newNodePtr;
+        QueueNode<T> newNode(value);
+        newNodePtr = &newNode;
+        
+        m_next = newNodePtr;
+        return m_next;
+    }
+    
+    QueueNode<T> *newNodePtr;
+    QueueNode<T> newNode(value);
+    newNode.m_next = m_next;
+    newNodePtr = &newNode;
+    
+    m_next = newNodePtr;
+    return m_next;
+}
+
+template<class T> void QueueNode<T>::setNext(QueueNode<T>* newNext) {
+    m_next = newNext;
+}
+
 
 // One function works for all data types.
 // This would work even for user defined types
